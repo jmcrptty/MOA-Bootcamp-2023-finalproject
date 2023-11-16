aba66389550d10db29f6779de7b614fb7ad58f94
jmcrptty <joseph.corputty@gmail.com>
2023-11-16 22:51:50 +0700

    first commit

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class=" mt-auto">
+    <p class="text-center border-top text-muted pt-3">©FINAL PROJECT 2023  </p>
+</footer>
---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+
+@include('inc.alert')
+<section>
+
+    <div class="container">
+        <div class="p-5 mb-4 bg-light rounded-3">
+            <div class="container-fluid py-5">
+                <h1 class="display-5 fw-bold">Dashboard</h1>
+                <p class="col-md-8 fs-4">
+                    Welcome to Manajemen Inventory
+                </p>
+            </div>
+        </div>
+        
+    </div>
+
+</section>
+@endsection
