9591f222b7346f30b766e1ee2c6edd6f3e04a620
jmcrptty <joseph.corputty@gmail.com>
2023-11-16 23:02:35 +0700

    up kedua

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,6 @@
 namespace Database\Seeders;
 
 
-use App\Models\Category;
-use App\Models\Item;
-use App\Models\Supplier;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,24 +15,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        $this->getCategories();
-
-        \App\Models\User::factory(2)->create();
-        Supplier::factory(2)->create();
-        // Category::factory(3)->create();
-        Item::factory(3)->create();
-        // Department::factory(2)->create();
         
     }
 
-    public function getCategories()
-    {
-        $Categoriesjson = file_get_contents(database_path() . '/categories.json');
-        $categories = json_decode($Categoriesjson, true)['categories'];
-
-        foreach ($categories as $category) {
-            Category::firstOrCreate($category);
-        }
-    }
+    
+    
 
 }
